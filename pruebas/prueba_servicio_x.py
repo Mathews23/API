fa9eaead0e9332445deae88619f8920af228f915
mathews diaz <mathewsdiaz@gmail.com>
2025-05-12 21:29:56 -0400

    fix: correct bearer token variable name for Twitter API authorization

--- pruebas/prueba_servicio_x.py
+++ pruebas/prueba_servicio_x.py
@@ -6,9 +6,22 @@ from os import getenv
 
 load_dotenv()
 
-bearertoken = getenv("BEARER_TOKEN")
+bearer_token = getenv("TWITTER_BEARER_TOKEN")
 url = "https://api.twitter.com/2/tweets/search/recent"
 
 headers = {
-    "Authorization": f"Bearer {bearertoken}"
+    "Authorization": f"Bearer {bearer_token}"
 }
+# Obtener equipos
+# Armar query
+    # Filtrar por equipos
+    # Filtrar por fecha
+query_string = {
+    "query": "from:elonmusk",
+    "tweet.fields": "public_metrics",
+    
+}
+response = requests.get(url, headers=headers, params=query_string)
+
+data = response.json()
+print(data)

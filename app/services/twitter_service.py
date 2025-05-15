20be79315726db9a82347e87b07c5947f6cb97aa
mathews diaz <mathewsdiaz@gmail.com>
2025-05-15 16:38:56 -0400

    refactor: streamline TwitterService initialization and enhance tweet retrieval method

--- app/services/twitter_service.py
+++ app/services/twitter_service.py
@@ -3,11 +3,27 @@ Servicio para interactuar con la API de Twitter usando Tweepy.
 Obtiene tweets de usuarios autenticándose con el Bearer Token.
 """
 from os import getenv
+import tweepy
+import asyncio
 
 class TwitterService:
     """
     Servicio para interactuar con la API de Twitter usando Tweepy.
     """
     def __init__(self):
-        self.headers = {
-            'Authorization': f'Bearer {getenv("TWITTER_BEARER_TOKEN")}' }
+        self.client = tweepy.Client(bearer_token=getenv("TWITTER_BEARER_TOKEN"))
+
+    async def get_tweets(self, query: str, max_results: int):
+        """
+        Obtiene tweets de Twitter de forma asíncrona según la consulta y el número máximo de resultados.
+        """
+        tweets = await asyncio.to_thread(self.client.search_recent_tweets, query=query, max_results=max_results)
+        return tweets
+
+    @staticmethod
+    def create_query(team: list[str], start_date: str, end_date: str):
+        """
+        Crea una consulta para buscar tweets de un equipo específico entre dos fechas.
+        """
+        query = f"from:{team} since:{start_date} until:{end_date}"
+        return query

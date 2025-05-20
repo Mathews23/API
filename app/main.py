035a745d3e9d4a8d81a9c1cccb77a3abadf67a31
mathews diaz <mathewsdiaz@gmail.com>
2025-05-20 09:03:25 -0400

    feat: add campaign and analytics routes to enhance application functionality

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ This module initializes the FastAPI application and defines the root endpoint.
 """
 
 from fastapi import FastAPI
-from app.routes import content_type, person, platform, client
+from app.routes import content_type, person, platform, client, campaign, analytics
 
 # Initialize the FastAPI application with lifespan
 app = FastAPI()
@@ -13,6 +13,8 @@ app.include_router(person.router, prefix="/people", tags=["people"])
 app.include_router(platform.router, prefix="/platforms", tags=["platforms"])
 app.include_router(client.router, prefix="/clients", tags=["clients"])
 app.include_router(content_type.router, prefix="/types", tags=["types"])
+app.include_router(campaign.router, prefix="/campaigns", tags=["campaigns"])
+app.include_router(analytics.router, prefix="/analytics", tags=["analytics"])
 
 # Access the Tweepy client in any endpoint
 @app.get("/")
